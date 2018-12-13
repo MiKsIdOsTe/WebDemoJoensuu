d389bb26feb4f63a7bad7d75e982364dd777cf9e
Minna Kakkonen <lenttis_94@hotmail.com>
2018-12-13 15:21:19 +0200

    Lisätty Asiakkaat ja luo asiakas

--- WebDemoJoensuu/Controllers/AsiakkaatController.cs
+++ WebDemoJoensuu/Controllers/AsiakkaatController.cs
@@ -14,7 +14,29 @@ namespace WebDemoJoensuu.Controllers
             NorthwindContext context = new NorthwindContext();
             List<Customers> asiakkaat = context.Customers.ToList();
 
+            /*Aina kun mennään asiakas indexiin niin luopi uusen asiakkaan oma firma oy. Joten ei hyvä!!!
+             * Customers uusi = new Customers()
+            {
+                CompanyName = "Oma firma Oy",
+                City = "Joensuu",
+                Country = "Finland"
+            };
+            context.Customers.Add(uusi);
+            context.SaveChanges();*/
+
             return View(asiakkaat);
+        }
+        public IActionResult LuoUusi()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult LuoUusi(Customers uusi)
+        {
+            NorthwindContext context = new NorthwindContext();
+            context.Customers.Add(uusi);
+            context.SaveChanges();
+            return View();
         }
     }
 }
